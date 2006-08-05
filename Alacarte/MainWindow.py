2daf3a3ceb477d3753887cb60b07a5bdb1916275
Travis Watkins <alleykat@gmail.com>
2006-08-05 04:26:45 +0000

    Fix crash caused by API change in gnome-menus (thanks Joe Marcus Clarke)
    006-08-04  Travis Watkins  <alleykat@gmail.com>
    
            * Fix crash caused by API change in gnome-menus (thanks Joe
              Marcus Clarke)

--- Alacarte/MainWindow.py
+++ Alacarte/MainWindow.py
@@ -67,8 +67,8 @@ class MainWindow:
 
 	def run(self):
 		self.loadMenus()
-		self.editor.applications.tree.add_monitor(self.menuChanged)
-		self.editor.settings.tree.add_monitor(self.menuChanged)
+		self.editor.applications.tree.add_monitor(self.menuChanged, None)
+		self.editor.settings.tree.add_monitor(self.menuChanged, None)
 		self.tree.get_widget('mainwindow').show_all()
 		gtk.main()
 

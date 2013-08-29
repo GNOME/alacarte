5c1cdb72587c0e1bf8e11aa4f0307b10da275358
Jasper St. Pierre <jstpierre@mecheye.net>
2013-08-29 18:41:48 -0400

    Remove unused imports

--- Alacarte/MainWindow.py
+++ Alacarte/MainWindow.py
@@ -20,11 +20,9 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GObject, Gio, GdkPixbuf, Gdk, GMenu, GLib
-import sys
 import cgi
 import os
 import gettext
-import subprocess
 import shutil
 
 from Alacarte import config

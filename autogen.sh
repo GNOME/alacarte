f609501337b17c4ec34e8fbbc3f08859d14ea94d
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2022-03-21 14:13:11 +0200

    build: don't use gnome-autogen.sh
    https://wiki.gnome.org/Projects/GnomeCommon/Migration

--- autogen.sh
+++ autogen.sh
@@ -1,44 +1,42 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
+srcdir=$(dirname "$0")
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="alacarte"
-REQUIRED_AUTOCONF_VERSION=2.53
-REQUIRED_AUTOMAKE_VERSION=${REQUIRED_AUTOMAKE_VERSION:-1.9}
-
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/autogen.sh) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
-
-DIE=0
-
-# This is a bit complicated here since we can't use gnome-config yet.
-# It'll be easier after switching to pkg-config since we can then
-# use pkg-config to find the gnome-autogen.sh script.
-
-gnome_autogen=
-gnome_datadir=
-
-ifs_save="$IFS"; IFS=":"
-for dir in $PATH ; do
-  test -z "$dir" && dir=.
-  if test -f $dir/gnome-autogen.sh ; then
-    gnome_autogen="$dir/gnome-autogen.sh"
-    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
-    break
-  fi
-done
-IFS="$ifs_save"
-
-if test -z "$gnome_autogen" ; then
-  echo "You need to install the gnome-common module and make"
-  echo "sure the gnome-autogen.sh script is in your \$PATH."
+if [ ! -f $srcdir/configure.ac ]; then
+  echo "**Error**: Directory "\'$srcdir\'" does not look like the top-level" \
+       "project directory."
   exit 1
 fi
 
-GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . $gnome_autogen
+olddir=$(pwd)
+cd "$srcdir"
+
+PKG_NAME=$(autoconf --trace 'AC_INIT:$1' "$srcdir/configure.ac")
+
+if [ "$#" = 0 ] && [ -z "$NOCONFIGURE" ]; then
+  echo "**Warning**: I am going to run 'configure' with no arguments." >&2
+  echo "If you wish to pass any to it, please specify them on the '$0'" \
+       "command line." >&2
+fi
+
+set -x
+autoreconf --verbose --force --install || exit 1
+{ set +x; } 2>/dev/null
+
+cd "$olddir"
+
+if [ -z "$NOCONFIGURE" ]; then
+  set -x
+  $srcdir/configure "$@" || exit 1
+  { set +x; } 2>/dev/null
+
+  if [ "$1" = "--help" ]; then
+    exit 0
+  else
+    echo "Now type 'make' to compile $PKG_NAME." || exit 1
+  fi
+else
+  echo "Skipping configure process."
+fi
